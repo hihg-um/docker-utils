c66c631fc1b52d65415b302cdb789564f2772a28
Sven-Thorsten Dietrich <sxd1425@med.miami.edu>
2024-01-11 13:17:12 -0500

    Update license to GPL-3.0
    Signed-off-by: Sven-Thorsten Dietrich <sxd1425@med.miami.edu>

--- Dockerfile.bash
+++ Dockerfile.bash
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-2.0
+# SPDX-License-Identifier: GPL-3.0
 ARG BASE_IMAGE
 FROM $BASE_IMAGE
 
--- src/test/git.sh
+++ src/test/git.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-3.0
 
 git --version
