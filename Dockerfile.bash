a5b2c72375830a0f92637783fb8988a9b693e2f3
Sven-Thorsten Dietrich <thebigcorporation@gmail.com>
2024-06-19 02:54:29 -0400

    Label improvements and build-cleanup
     - don't time-stamp the base layer to avoid rebuilds
     - label build time and title in images
     - simplify test-script installation
     - simplify entrypoint
    
    Signed-off-by: Sven-Thorsten Dietrich <thebigcorporation@gmail.com>

--- Dockerfile.bash
+++ Dockerfile.bash
@@ -6,21 +6,21 @@ FROM $BASE
 ARG BASE
 ARG RUN_CMD
 ARG BUILD_REPO
+ARG BUILD_TIME
 
-RUN apt -y update -qq && \
-    DEBIAN_FRONTEND=noninteractive apt -y install \
-    --no-install-recommends --no-install-suggests \
-    bash \
-    && \
+RUN apt -y update -qq && DEBIAN_FRONTEND=noninteractive apt -y install \
+	--no-install-recommends --no-install-suggests \
+		bash \
+	&& \
 	apt -y clean && rm -rf /var/lib/apt/lists/* /tmp/*
 
-ARG TEST="/test.sh"
-COPY --chmod=0555 src/test/$RUN_CMD.sh ${TEST}
+COPY --chmod=0555 src/test/${RUN_CMD}.sh /test.sh
 
-ARG ENTRY="/entrypoint.sh"
-RUN echo "#!/bin/bash\n$RUN_CMD \$@" > ${ENTRY} && chmod ugo+rx ${ENTRY}
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENTRYPOINT [ "bash -c" ]
 
+LABEL org.opencontainers.image.base.digest=""
 LABEL org.opencontainers.image.base.name="${BASE}"
-LABEL org.opencontainers.image.description="Utilities: ${RUN_CMD}"
+LABEL org.opencontainers.image.created="$BUILD_TIME"
+LABEL org.opencontainers.image.description="Docker Utilities: ${RUN_CMD}"
+LABEL org.opencontainers.image.title="${RUN_CMD}"
 LABEL org.opencontainers.image.url="${BUILD_REPO}"
